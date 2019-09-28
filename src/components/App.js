3ad06853f9b8061e7a9d2485fac313f1779ba2e2
Francisco Molina <molinajimenez01@gmail.com>
2019-09-27 22:55:05 -0600

    Articles "terminados"?

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,10 @@ class App extends Component{
             <div>
                 <Header></Header>
                 <VideoFrame></VideoFrame>
-                <Article firstTag="Selected projects" secondTag="Good fun, great work" thirdTag="LOVESPACE" fourthTag="Improving the UX for 50,000 customers a year"></Article>
+                <Article titles={["Selected projects","LOVESPACE", "Payaca", "Bunk"]} subtitles={["Good fun, great work", 
+                "Improving the UX for 50,000 customers a year", 
+                "Leveling the playing field with a new brand, website and app",
+                "Putting renting in motion"]} colors={["#4CBBB6", "#FFE18B","#46C4F3"]}></Article>
             </div>
        );
     }
--- src/components/Article.js
+++ src/components/Article.js
@@ -11,14 +11,14 @@ class Article extends Component{
             <section>
                 <div className="container-video-Upper">
                     <div className="headerArt1">
-                        <h2 className="mainTitle">{this.props.firstTag}</h2>
-                        <h3 className="subtitle">{this.props.secondTag}</h3>
+                        <h2 className="mainTitle">{this.props.titles[0]}</h2>
+                        <h3 className="subtitle">{this.props.subtitles[0]}</h3>
                     </div>
                     <ul className="items">
                         <li className="item1">
                             <a href="/#" title="LOVESPACE">
                                 <div className="wrapper-item">
-                                    <div className="work-item">
+                                    <div className="work-item" style={{backgroundColor: this.props.colors[0]}}>
                                         <img className="image" src={require("../img/img1.png")} alt="cosa"></img>
                                     </div>
 
@@ -27,15 +27,61 @@ class Article extends Component{
 
                             <div className="inGrid">
                                 <div className="info">
-                                    <h3 className="mainTitle">{this.props.thirdTag}</h3>
-                                    <h4 className="subtitle">{this.props.fourthTag}</h4>
+                                    <h3 className="mainTitle">{this.props.titles[1]}</h3>
+                                    <h4 className="subtitle">{this.props.subtitles[1]}</h4>
                                 </div>
 
                                 <div className="buttonHolder">
                                     <CoolButton class="animated" text='Read more'></CoolButton>
                                 </div>    
                             </div>
-                            
+                        </li>
+
+                        <li className="item1">
+
+                            <a href="/#" title="LOVESPACE">
+                                <div className="wrapper-item">
+                                    <div className="work-item" style={{backgroundColor: this.props.colors[1]}}>
+                                        <img className="image" src={require("../img/img2.png")} alt="cosa"></img>
+                                    </div>
+
+                                </div>
+                            </a>
+
+                            <div className="inGrid">
+                                <div className="info">
+                                    <h3 className="mainTitle">{this.props.titles[2]}</h3>
+                                    <h4 className="subtitle">{this.props.subtitles[2]}</h4>
+                                </div>
+
+                                <div className="buttonHolder">
+                                    <CoolButton class="animated" text='Read more'></CoolButton>
+                                </div>    
+                            </div>
+
+                        </li>
+                         <li className="item1">
+
+                            <a href="/#" title="LOVESPACE">
+                                <div className="wrapper-item">
+                                    <div className="work-item" style={{backgroundColor: this.props.colors[2]}}>
+                                        <img className="image" src={require("../img/img3.png")} alt="cosa"></img>
+                                    </div>
+
+                                </div>
+                            </a>
+
+                            <div className="inGrid">
+                                <div className="info">
+                                    <h3 className="mainTitle">{this.props.titles[3]}</h3>
+                                    <h4 className="subtitle">{this.props.subtitles[2]}</h4>
+                                </div>
+
+                                <div className="buttonHolder">
+                                    <CoolButton class="animated" text='Read more'></CoolButton>
+                                </div>    
+                            </div>
+
                         </li>
                     </ul>
                 </div>
